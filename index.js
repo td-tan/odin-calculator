a614f5bc1ff4890b6c50cc0aeb4d5ff8856a9217
td-tan <47648230+td-tan@users.noreply.github.com>
2023-01-23 21:06:32 +0100

    feat: impl operator buttons

--- index.js
+++ index.js
@@ -13,16 +13,25 @@ function operate(op, x, y) {
 }
 
 const numberBtns = document.querySelectorAll('button[data-item="number"]');
+const operatorBtns = document.querySelectorAll('button[data-item="operator"]');
 const display = document.querySelector('#display');
+
 let number = 0;
+let operator = '';
+
 numberBtns.forEach((numberBtn) => {
     numberBtn.addEventListener('click', e => {
-        number = Number(e.target.textContent);
-
         // Reset display by 0
         if (display.textContent === '0') {
             display.textContent = '';
         }
-        display.textContent += number;
+        display.textContent += e.target.textContent;
+    });
+});
+
+operatorBtns.forEach((operatorBtn) => {
+    operatorBtn.addEventListener('click', (e) => {
+        operator = e.target.textContent;
+        number = Number(display.textContent);
     });
 });
