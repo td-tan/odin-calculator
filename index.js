ab8138ea06deab44e049cff918370f55721f5b6f
td-tan <47648230+td-tan@users.noreply.github.com>
2023-01-23 20:35:31 +0100

    feat: impl func that takes operatore and two operands

---
+++ index.js
@@ -0,0 +1,13 @@
+function operate(op, x, y) {
+    switch(op) {
+        case '+':
+            return x + y;
+        case '-':
+            return x - y;
+        case '*':
+            return x * y;
+        case '/':
+            return x / y;
+    }
+    return NaN;
+}
