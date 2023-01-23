ea4d79940a9eeb7f422b08bde07aa2d05b617879
td-tan <47648230+td-tan@users.noreply.github.com>
2023-01-23 21:30:34 +0100

    feat: split display to previous and current value display

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function operate(op, x, y) {
 const numberBtns = document.querySelectorAll('button[data-item="number"]');
 const operatorBtns = document.querySelectorAll('button[data-item="operator"]');
 const enterBtn = document.querySelector('button[data-item="enter"]');
-const display = document.querySelector('#display');
+const currentDisplay = document.querySelector('#current');
 
 let number = 0;
 let number2 = 0;
@@ -23,23 +23,23 @@ let operator = '';
 
 numberBtns.forEach((numberBtn) => {
     numberBtn.addEventListener('click', e => {
-        // Reset display by 0
-        if (display.textContent === '0') {
-            display.textContent = '';
+        // Reset currentDisplay by 0
+        if (currentDisplay.textContent === '0') {
+            currentDisplay.textContent = '';
         }
-        display.textContent += e.target.textContent;
+        currentDisplay.textContent += e.target.textContent;
     });
 });
 
 operatorBtns.forEach((operatorBtn) => {
     operatorBtn.addEventListener('click', e => {
         operator = e.target.textContent;
-        number = Number(display.textContent);
-        display.textContent = '0';
+        number = Number(currentDisplay.textContent);
+        currentDisplay.textContent = '0';
     });
 });
 
 enterBtn.addEventListener('click', () => {
-    number2 = Number(display.textContent);
-    display.textContent = operate(operator, number, number2);
+    number2 = Number(currentDisplay.textContent);
+    currentDisplay.textContent = operate(operator, number, number2);
 });
